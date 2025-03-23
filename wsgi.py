e080727ef9e9dfba5edd23cd7e9a29488d58d180
Mbogneng-Junior <mbognengj@gmail.com>
2025-03-23 09:41:33 +0100

    okay

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,16 @@
-from app import server
+"""WSGI entry point for the Dash application"""
+import os
+import sys
 
-app = server
+# Ajouter le répertoire courant au PYTHONPATH
+current_dir = os.path.dirname(os.path.abspath(__file__))
+if current_dir not in sys.path:
+    sys.path.append(current_dir)
+
+from app import server as application
+
+# Pour Gunicorn
+app = application
 
 if __name__ == '__main__':
     app.run()
